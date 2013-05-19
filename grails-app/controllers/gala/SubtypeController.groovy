f928345a256aa18d3341a6aedf89f1d2f636fa4a
rasom <rasom@ukr.net>
2013-05-19 22:19:11 +0300

    "_"

--- grails-app/controllers/gala/SubtypeController.groovy
+++ grails-app/controllers/gala/SubtypeController.groovy
@@ -17,18 +17,38 @@ class SubtypeController {
     def save(Long id){
         def segment = Segment.get(id)
         def type = Type.get(params.typeId)
-        println params
         def props = [
             type: type, 
             segment: segment, 
             pointsCount: params.pointsCount,
-            block: params.block,
-            restore: params.restore] 
+            block: params.block != null,
+            restore: params.restore != null] 
         def subtype = new Subtype(props)
         if(!subtype.save()){
             throw new Exception("subtype wasnt saved!!!");
-        }
+        } 
         
+        def resp = [result: "success"]
+        render resp as JSON
+    }
+    
+    def update(Long id){
+        def subtype = Subtype.get(params.id)
+        subtype.pointsCount = Integer.parseInt(params.pointsCount)
+        subtype.block = params.block
+        subtype.restore = params.restore
+        
+        if(!subtype.save()){
+            throw new Exception("subtype wasnt updated!!!");
+        }
+        println 'subtype updated'
+        def resp = [result: "success"]
+        render resp as JSON
+    }
+    
+    def remove(Long id){
+        Subtype.get(params.id).delete()
+        println 'subtype removed'
         def resp = [result: "success"]
         render resp as JSON
     }
